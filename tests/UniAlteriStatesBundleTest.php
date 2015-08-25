87541fdce210f6e40aa25030f2b363570118c49f
Richard Deloge <r.deloge@uni-alteri.com>
2015-08-26 00:14:32 +0200

    fix tests

--- tests/UniAlteriStatesBundleTest.php
+++ tests/UniAlteriStatesBundleTest.php
@@ -65,55 +65,15 @@ class UniAlteriStatesBundleTest extends \PHPUnit_Framework_TestCase
         $bundle = new UniAlteriStatesBundle();
         $bundle->setContainer($this->_container);
 
-        $bundle->boot();
+        $eventManagerMock = $this->getMock('Doctrine\Common\EventManager', [], [], '', false);
+        $eventManagerMock->expects($this->once())
+            ->method('addEventListener')
+            ->willReturnCallback(function ($name, $args) {
+                $this->assertEquals('loadClassMetadata', $name);
+                $this->assertInstanceOf('UniAlteri\Bundle\StatesBundle\Doctrine\LoadClassMetaListener', $args);
+            });
 
-        $this->assertTrue($this->_container->has('unialteri.states.loader'));
-        $loader = $this->_container->get('unialteri.states.loader');
-
-        //Check if the loader implements the good interface
-        $this->assertInstanceOf('\\UniAlteri\\States\\Loader\\LoaderInterface', $loader);
-
-        //Check if the loader is initialized with a di container
-        $container = $loader->getDIContainer();
-        $this->assertInstanceOf('\\UniAlteri\\States\\DI\\ContainerInterface', $container);
-
-        //Check if required services are present into the di container
-        $this->assertTrue($container->testEntry(Loader\FinderInterface::DI_FINDER_SERVICE));
-        $this->assertTrue($container->testEntry(States\States\StateInterface::INJECTION_CLOSURE_SERVICE_IDENTIFIER));
-
-        $fail = false;
-        try {
-            $container->get(Loader\FinderInterface::DI_FINDER_SERVICE);
-        } catch (Exception\UnavailableFactory $e) {
-            $fail = true;
-        } catch (\Exception $e) {
-        }
-
-        $this->assertTrue($fail, 'Error, the service to create finder must throw exception if the DI Container for the class has not registered factory object');
-
-        //Test behavior of the service to create finder for a stated class
-        $container->registerInstance(Factory\FactoryInterface::DI_FACTORY_NAME, new Support\MockFactory());
-        $finder = $container->get(Loader\FinderInterface::DI_FINDER_SERVICE);
-        $this->assertInstanceOf('\\UniAlteri\\States\\Loader\\FinderInterface', $finder);
-
-        //Test behavior of the service to create injection closure
-        $injectionClosure = $container->get(States\States\StateInterface::INJECTION_CLOSURE_SERVICE_IDENTIFIER);
-        $this->assertInstanceOf('\\UniAlteri\\States\\DI\\InjectionClosureInterface', $injectionClosure);
-    }
-
-    public function testLoaderInitialisationWithoutFlocOperator()
-    {
-        if ('5.6' > PHP_VERSION) {
-            $this->markTestSkipped('Version of PHP is not supported for this injection closure');
-
-            return;
-        }
-
-        defined('DISABLE_PHP_FLOC_OPERATOR') || define('DISABLE_PHP_FLOC_OPERATOR', true);
-
-        //Initialize container
-        $bundle = new UniAlteriStatesBundle();
-        $bundle->setContainer($this->_container);
+        $this->_container->set('doctrine.event_listener', $eventManagerMock);
 
         $bundle->boot();
 
@@ -123,112 +83,12 @@ class UniAlteriStatesBundleTest extends \PHPUnit_Framework_TestCase
         //Check if the loader implements the good interface
         $this->assertInstanceOf('\\UniAlteri\\States\\Loader\\LoaderInterface', $loader);
 
-        //Check if the loader is initialized with a di container
-        $container = $loader->getDIContainer();
-        $this->assertInstanceOf('\\UniAlteri\\States\\DI\\ContainerInterface', $container);
+        $this->assertInstanceOf('\\Closure', $loader->getFinderFactory());
 
-        //Check if required services are present into the di container
-        $this->assertTrue($container->testEntry(Loader\FinderInterface::DI_FINDER_SERVICE));
-        $this->assertTrue($container->testEntry(States\States\StateInterface::INJECTION_CLOSURE_SERVICE_IDENTIFIER));
-
-        $fail = false;
-        try {
-            $container->get(Loader\FinderInterface::DI_FINDER_SERVICE);
-        } catch (Exception\UnavailableFactory $e) {
-            $fail = true;
-        } catch (\Exception $e) {
-        }
-
-        $this->assertTrue($fail, 'Error, the service to create finder must throw exception if the DI Container for the class has not registered factory object');
-
-        //Test behavior of the service to create finder for a stated class
-        $container->registerInstance(Factory\FactoryInterface::DI_FACTORY_NAME, new Support\MockFactory());
-        $finder = $container->get(Loader\FinderInterface::DI_FINDER_SERVICE);
+        $finder = $loader->getFinderFactory()->__invoke('class', 'path');
         $this->assertInstanceOf('\\UniAlteri\\States\\Loader\\FinderInterface', $finder);
-
-        //Test behavior of the service to create injection closure
-        $injectionClosure = $container->get(States\States\StateInterface::INJECTION_CLOSURE_SERVICE_IDENTIFIER);
-        $this->assertInstanceOf('\\UniAlteri\\States\\DI\\InjectionClosureInterface', $injectionClosure);
-    }
-
-    public function testLoaderBehaviorIfComposerIsNotAvailable()
-    {
-        //Fake autoload method to simulate an not empty autoload stack
-        spl_autoload_register(function ($className) {return false;});
-
-        //Initialize container
-        $bundle = new UniAlteriStatesBundle();
-        $bundle->setContainer($this->_container);
-
-        //Remove autoloader
-        $autoloadCallbackList = \spl_autoload_functions();
-
-        $composerAutoloaderCallback = null;
-        if (!empty($autoloadCallbackList)) {
-            foreach ($autoloadCallbackList as $autoloadCallback) {
-                if (is_array($autoloadCallback) && isset($autoloadCallback[0])
-                    && $autoloadCallback[0] instanceof ClassLoader
-                ) {
-                    $composerAutoloaderCallback = $autoloadCallback;
-                    spl_autoload_unregister($autoloadCallback);
-                }
-            }
-        }
-
-        try {
-            $bundle->boot();
-        } catch (\RuntimeException $e) {
-            if (is_callable($composerAutoloaderCallback)) {
-                spl_autoload_register($composerAutoloaderCallback, true, true);
-            }
-
-            return;
-        } catch (\Exception $e) { /* ... */
-        }
-
-        $this->fail('Error, the boot method must throw an exception when the Composer Loader is not available');
-
-        if (is_callable($composerAutoloaderCallback)) {
-            spl_autoload_register($composerAutoloaderCallback, true, true);
-        }
-    }
-
-    public function testLoaderBehaviorIfComposerIsNotAvailableEmpty()
-    {
-        //Initialize container
-        $bundle = new UniAlteriStatesBundle();
-        $bundle->setContainer($this->_container);
-
-        //Remove autoloader
-        $autoloadCallbackList = \spl_autoload_functions();
-
-        $composerAutoloaderCallback = null;
-        if (!empty($autoloadCallbackList)) {
-            foreach ($autoloadCallbackList as $autoloadCallback) {
-                if (is_array($autoloadCallback) && isset($autoloadCallback[0])
-                    && $autoloadCallback[0] instanceof ClassLoader
-                ) {
-                    $composerAutoloaderCallback = $autoloadCallback;
-                    spl_autoload_unregister($autoloadCallback);
-                }
-            }
-        }
-
-        try {
-            $bundle->boot();
-        } catch (\RuntimeException $e) {
-            if (is_callable($composerAutoloaderCallback)) {
-                spl_autoload_register($composerAutoloaderCallback, true, true);
-            }
-
-            return;
-        } catch (\Exception $e) { /* ... */
-        }
-
-        $this->fail('Error, the boot method must throw an exception when the Composer Loader is not available');
-
-        if (is_callable($composerAutoloaderCallback)) {
-            spl_autoload_register($composerAutoloaderCallback, true, true);
+        if ($finder instanceof Loader\FinderInterface) {
+            $this->assertEquals('class', $finder->getStatedClassName());
         }
     }
 }
