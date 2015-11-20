9cb5e0d1b9e549f944dade27919dc341375ebe7b
Richard Deloge <r.deloge@uni-alteri.com>
2015-11-21 00:28:32 +0100

    fix bug in composer finder in 1.1 version

--- tests/Support/IntegratedDocument.php
+++ tests/Support/IntegratedDocument.php
@@ -11,14 +11,14 @@
  * obtain it through the world-wide-web, please send an email
  * to contact@uni-alteri.com so we can send you a copy immediately.
  *
- * @copyright   Copyright (c) 2009-2016 Uni Alteri (http://uni-alteri.com)
- * @copyright   Copyright (c) 2009-2016 Richard Déloge (r.deloge@uni-alteri.com)
+ *
+ * @copyright   Copyright (c) 2009-2016 Richard Déloge (richarddeloge@gmail.com)
  *
  * @link        http://teknoo.it/states Project website
  *
  * @license     http://teknoo.it/states/license/mit         MIT License
  * @license     http://teknoo.it/states/license/gpl-3.0     GPL v3 License
- * @author      Richard Déloge <r.deloge@uni-alteri.com>
+ * @author      Richard Déloge <richarddeloge@gmail.com>
  */
 
 namespace Teknoo\Tests\Bundle\StatesBundle\Support;
@@ -35,14 +35,14 @@ use Teknoo\Bundle\StatesBundle\Document\IntegratedDocument as AbstractIntegrated
  * To avoid errors in the usage of this lib, these interfaces are not defined with released proxies.
  * You must implement these interface, according to your needs, in your derived proxies like in this class.
  *
- * @copyright   Copyright (c) 2009-2016 Uni Alteri (http://uni-alteri.com)
- * @copyright   Copyright (c) 2009-2016 Richard Déloge (r.deloge@uni-alteri.com)
+ *
+ * @copyright   Copyright (c) 2009-2016 Richard Déloge (richarddeloge@gmail.com)
  *
  * @link        http://teknoo.it/states Project website
  *
  * @license     http://teknoo.it/states/license/mit         MIT License
  * @license     http://teknoo.it/states/license/gpl-3.0     GPL v3 License
- * @author      Richard Déloge <r.deloge@uni-alteri.com>
+ * @author      Richard Déloge <richarddeloge@gmail.com>
  */
 class IntegratedDocument extends AbstractIntegratedDocument implements
     \Serializable,
