73cb4689a406b9bc1b7d474e0d07f73decdc2c07
Richard Deloge <richarddeloge@gmail.com>
2016-11-02 00:12:22 +0100

    Convertir this bundle into an universal package, respecting the PSR11 draft and container-interop/definition-interop to be use with any framework

---
+++ tests/universal/DefinitionProviderTest.php
@@ -0,0 +1,57 @@
+<?php
+
+/**
+ * StatesBundle.
+ *
+ * LICENSE
+ *
+ * This source file is subject to the MIT license and the version 3 of the GPL3
+ * license that are bundled with this package in the folder licences
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to richarddeloge@gmail.com so we can send you a copy immediately.
+ *
+ *
+ * @copyright   Copyright (c) 2009-2016 Richard Déloge (richarddeloge@gmail.com)
+ *
+ * @link        http://teknoo.software/states Project website
+ *
+ * @license     http://teknoo.software/license/mit         MIT License
+ * @author      Richard Déloge <richarddeloge@gmail.com>
+ */
+namespace Teknoo\Tests\UniversalPackage\States;
+
+use Teknoo\States\LifeCycle\Event\EventDispatcherBridgeInterface;
+use Teknoo\States\LifeCycle\Observing\ObservedFactoryInterface;
+use Teknoo\States\LifeCycle\Observing\ObserverInterface;
+use Teknoo\States\LifeCycle\Scenario\ManagerInterface;
+use Teknoo\States\LifeCycle\Tokenization\TokenizerInterface;
+use Teknoo\UniversalPackage\States\DefinitionProvider;
+
+/**
+ * Class DefinitionProviderTest
+ * @covers \Teknoo\UniversalPackage\States\DefinitionProvider
+ */
+class DefinitionProviderTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @return DefinitionProvider
+     */
+    public function buildProvider(): DefinitionProvider
+    {
+        return new DefinitionProvider();
+    }
+
+    public function testGetDefinitions()
+    {
+        $definitions = $this->buildProvider()->getDefinitions();
+        self::assertTrue(isset($definitions[TokenizerInterface::class]));
+        self::assertTrue(isset($definitions[EventDispatcherBridgeInterface::class]));
+        self::assertTrue(isset($definitions[ManagerInterface::class]));
+        self::assertTrue(isset($definitions[ObservedFactoryInterface::class]));
+        self::assertTrue(isset($definitions[ObserverInterface::class]));
+        self::assertTrue(isset($definitions['teknoo.vendor.yaml.parser']));
+        self::assertTrue(isset($definitions['teknoo.vendor.service.gaufrette.adapter']));
+        self::assertTrue(isset($definitions['teknoo.vendor.service.gaufrette.filesystem']));
+    }
+}
